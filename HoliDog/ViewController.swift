2fcad75296cddead4a00b39bf9e8e2cc0e01d4a8
Carles Mitjans <mitjans.coma@gmail.com>
2018-12-13 19:08:49 +0100

    Added Picker View and breeds

--- HoliDog/ViewController.swift
+++ HoliDog/ViewController.swift
@@ -14,25 +14,40 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var fetchDogButton: UIButton!
     @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var currentBreedView: UIView!
+    @IBOutlet weak var breedButton: UIButton!
+    @IBOutlet weak var breedPickerView: UIPickerView!
+    
+    var breeds: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        fetchDogButton.layer.cornerRadius = 10
-        fetchDogButton.clipsToBounds = true
-        
+        setupUI()
+        fetchBreedsList()
         fetchNewDog()
     }
     
+    func setupUI() {
+        breedPickerView.isHidden = true
+        breedPickerView.delegate = self
+        breedButton.contentHorizontalAlignment = .left
+        breedButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        dogImageView.layer.cornerRadius = 5
+        dogImageView.clipsToBounds = true
+        currentBreedView.layer.cornerRadius = 5
+        currentBreedView.clipsToBounds = true
+        fetchDogButton.layer.cornerRadius = 10
+        fetchDogButton.clipsToBounds = true
+    }
     
     func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
     
-    func dataToDictionary(data: Data?) -> [String: String]? {
+    func dataToDictionary(data: Data?) -> [String: Any]? {
         if let data = data {
             do {
-                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: String]
+                return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
             } catch {
                 print("Error!")
             }
@@ -49,8 +64,28 @@ class ViewController: UIViewController {
         SVProgressHUD.show()
         getData(from: URL(string: "https://dog.ceo/api/breeds/image/random")!) { (optionalData, optionalResponse, optionalError) in
             if let dict = self.dataToDictionary(data: optionalData) {
-                let dogImageURL = URL(string: dict["message"]!)!
+                let dogImageURL = URL(string: dict["message"]! as! String)!
                 self.downloadImage(from: dogImageURL)
+            }
+        }
+    }
+    
+    func fetchBreedsList() {
+        SVProgressHUD.show()
+        getData(from: URL(string: "https://dog.ceo/api/breeds/list/all")!) { (optionalData, optionalResponse, optionalError) in
+            if let dict = self.dataToDictionary(data: optionalData), let breedDict = dict["message"]! as? [String: [String]] {
+                for (breed, subBreed) in breedDict {
+                    if subBreed.isEmpty {
+                        self.breeds.append(breed.capitalizingFirstLetter())
+                    } else {
+                        for sub in subBreed {
+                            self.breeds.append("\(sub.capitalizingFirstLetter()) \(breed.capitalizingFirstLetter())")
+                        }
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                self.breedPickerView.reloadAllComponents()
             }
         }
     }
@@ -63,6 +98,30 @@ class ViewController: UIViewController {
                 SVProgressHUD.dismiss()
                 self.fetchDogButton.isEnabled = true
             }
+        }
+    }
+}
+
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return breeds.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let name = breeds[row]
+        return NSAttributedString(string: name, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+    }
+    
+    
+    @IBAction func breedPickerButtonPressed(_ sender: UIButton) {
+        breedPickerView.isHidden = false
+        UIView.animate(withDuration: 0.2) {
+            self.breedPickerView.frame = CGRect(x: 0, y: self.view.frame.height - self.breedPickerView.frame.height, width: self.breedPickerView.frame.width, height: self.breedPickerView.frame.height)
         }
     }
 }
